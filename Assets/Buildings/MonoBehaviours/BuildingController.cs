c86fdbfe33c3258085fac5ed4a41dae3285defe1
JonhyBee <jean.brunelle.1@gmail.com>
2023-05-12 23:53:33 -0400

    Fixed my buttons! fully working as intended now, even cancels if the building is obstructed

--- Assets/Buildings/MonoBehaviours/BuildingController.cs
+++ Assets/Buildings/MonoBehaviours/BuildingController.cs
@@ -9,27 +9,40 @@ public class BuildingController : MonoBehaviour
   public LayerMask obstacleLayers; // a layer mask to determine which layers should be considered obstacles
 
   private int selectedBuildingIndex; // the index of the currently selected building
-  private GameObject ghostObject; // the currently active ghost object
   private bool isPlacingBuilding; // whether or not the player is currently placing a building
+
+  private GameObject[] ghostObjects;
 
   void Start()
   {
+    // Initialize the array of ghost objects
+    ghostObjects = new GameObject[buildingPrefabs.Length];
+
     // set up the building selection buttons
     for (int i = 0; i < buildingButtons.Length; i++)
     {
-      int index = i; // need to create a local variable here to avoid closure issues
+      int index = i; // create a local variable here to avoid closure issues
       buildingButtons[i].image.sprite = buildingIcons[i];
-      buildingButtons[i].onClick.AddListener(() => {
-      SelectBuilding(index);
-      });
-    }
+      buildingButtons[i].onClick.AddListener(() => SelectBuilding(index));
 
-    // set the first building in the array as the default selection
-    SelectBuilding(0);
+      // Create a ghost object for each building prefab
+      ghostObjects[i] = Instantiate(buildingPrefabs[i], Vector3.zero, Quaternion.identity);
+      ghostObjects[i].SetActive(false);
+
+      Debug.Log($"Created ghost object for building {i}: {ghostObjects[i]}");
+    }
   }
+
+
 
   void Update()
   {
+    // if the player is currently placing a building, update the position of the ghost object
+    if (isPlacingBuilding)
+    {
+      UpdateGhostObjectPosition();
+    }
+
     // check if the player has clicked the mouse button
     if (Input.GetMouseButtonDown(0))
     {
@@ -38,52 +51,62 @@ public class BuildingController : MonoBehaviour
       {
         TryPlaceBuilding();
       }
-      // otherwise, start placing the selected building
-      else
-      {
-       return;
-      }
-    }
-
-    // if the player is currently placing a building, update the position of the ghost object
-    if (isPlacingBuilding)
-    {
-      UpdateGhostObjectPosition();
     }
   }
 
-  // selects a building from the array and updates the ghost object to match its shape
   void SelectBuilding(int index)
   {
+    Debug.Log($"Selecting building {index}: {ghostObjects[index]}");
+
+    // if a building is currently being placed, stop placing it
+    if (isPlacingBuilding)
+    {
+      ghostObjects[selectedBuildingIndex].SetActive(false);
+    }
+
     selectedBuildingIndex = index;
-    ghostObject = Instantiate(buildingPrefabs[index], Vector3.zero, Quaternion.identity);
-    ghostObject.GetComponent<SpriteRenderer>().sprite = buildingPrefabs[index].GetComponent<SpriteRenderer>().sprite;
-    ghostObject.SetActive(true);
+    ghostObjects[selectedBuildingIndex].SetActive(true);
     isPlacingBuilding = true;
   }
 
-  // tries to place the selected building at the current ghost object position
+
   void TryPlaceBuilding()
   {
-    if (!IsGhostObjectObstructed())
+    if (IsGhostObjectObstructed())
     {
-      Instantiate(buildingPrefabs[selectedBuildingIndex], ghostObject.transform.position, Quaternion.identity);
-      Destroy(ghostObject);
+      ghostObjects[selectedBuildingIndex].SetActive(false);
+      isPlacingBuilding = false;
+    }
+    else
+    {
+      Instantiate(buildingPrefabs[selectedBuildingIndex], ghostObjects[selectedBuildingIndex].transform.position, Quaternion.identity);
+      ghostObjects[selectedBuildingIndex].SetActive(false);
       isPlacingBuilding = false;
     }
   }
+
+
 
   // updates the position of the ghost object to match the mouse position
   void UpdateGhostObjectPosition()
   {
     Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    ghostObject.transform.position = new Vector3(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y), 0);
+    ghostObjects[selectedBuildingIndex].transform.position = new Vector3(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y), 0);
   }
 
-  // checks if the current ghost object position is obstructed by any colliders in the scene
   bool IsGhostObjectObstructed()
   {
-    Collider2D[] colliders = Physics2D.OverlapBoxAll(ghostObject.transform.position, ghostObject.GetComponent<BoxCollider2D>().size, 0, obstacleLayers);
-    return colliders.Length > 0;
+    BoxCollider2D ghostCollider = ghostObjects[selectedBuildingIndex].GetComponent<BoxCollider2D>();
+    Vector2 size = ghostCollider.size * ghostCollider.transform.localScale;
+    Collider2D[] colliders = Physics2D.OverlapBoxAll(ghostObjects[selectedBuildingIndex].transform.position, size, 0, obstacleLayers);
+    foreach (Collider2D collider in colliders)
+    {
+      // Ignore the ghost object's own collider
+      if (collider.gameObject != ghostObjects[selectedBuildingIndex])
+        return true;
+    }
+    return false;
   }
+
+
 }
